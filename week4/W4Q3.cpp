af8f4e9dc242770e4d8994e03ae597d803610d2d
KyatramoniRakesh <93992093+KyatramoniRakesh@users.noreply.github.com>
2021-11-14 21:38:48 +0530

    Update W4Q3.cpp

--- week4/W4Q3.cpp
+++ week4/W4Q3.cpp
@@ -1,113 +1,77 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@KyatramoniRakesh 
-akshitasinghal4444
-/
-DAA-lab
-Public
-1
-02
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-DAA-lab/week-4/k_smallest.cpp
-@akshitasinghal4444
-akshitasinghal4444 Add files via upload
-Latest commit e36e45c on Sep 1
- History
- 1 contributor
-70 lines (61 sloc)  1.1 KB
-   
-#include<iostream>
-#include<cstdlib>
-using namespace std;
+	#include<cstdlib>
+	using namespace std;
+	
 
-void swap(int *n1,int *n2)
-{
-    int t=*n1;
-    *n1=*n2;
-    *n2=t;
-}
+	void swap(int *n1,int *n2)
+	{
+	    int t=*n1;
+	    *n1=*n2;
+	    *n2=t;
+	}
+	
 
-int partition(int *a,int l,int r)
-{
-    int x=rand()%(r-l+1)+l;
-    swap(&a[x],&a[r]);
-    int pivot=a[r];
-    int i,j;
-    for(i=l-1,j=l;j<r;j++)
-    {
-        if(a[j]<pivot)
-        {
-            i++;
-            swap(&a[i],&a[j]);
-        }
-    }
-    swap(&a[i+1],&a[r]);
-    return (i+1);
-}
+	int partition(int *a,int l,int r)
+	{
+	    int x=rand()%(r-l+1)+l;
+	    swap(&a[x],&a[r]);
+	    int pivot=a[r];
+	    int i,j;
+	    for(i=l-1,j=l;j<r;j++)
+	    {
+	        if(a[j]<pivot)
+	        {
+	            i++;
+	            swap(&a[i],&a[j]);
+	        }
+	    }
+	    swap(&a[i+1],&a[r]);
+	    return (i+1);
+	}
+	
 
-int quick_sort(int *a,int l,int r,int k)
-{
-    if(l<r)
-    {
-        int pivot=partition(a,l,r);
-        if(pivot==k)
-        return a[pivot];
-        if(k<pivot)
-        return quick_sort(a,l,pivot-1,k);
-        else
-        return quick_sort(a,pivot+1,r,k);
-    }
-    return -1;
-}
+	int quick_sort(int *a,int l,int r,int k)
+	{
+	    if(l<r)
+	    {
+	        int pivot=partition(a,l,r);
+	        if(pivot==k)
+	        return a[pivot];
+	        if(k<pivot)
+	        return quick_sort(a,l,pivot-1,k);
+	        else
+	        return quick_sort(a,pivot+1,r,k);
+	    }
+	    return -1;
+	}
+	
 
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,k;
-        cin>>n;
+	int main()
+	{
+	    int t;
+	    cin>>t;
+	    while(t--)
+	    {
+	        int n,k;
+	        cin>>n;
+	
 
-        int i;
-        int a[n];
-        for(i=0;i<n;i++)
-        cin>>a[i];
+	        int i;
+	        int a[n];
+	        for(i=0;i<n;i++)
+	        cin>>a[i];
+	
 
-        cin>>k;
+	        cin>>k;
+	
 
-        int ans=quick_sort(a,0,n-1,k-1);
+	        int ans=quick_sort(a,0,n-1,k-1);
+	
 
-        if(ans==-1)
-        cout<<"not present\n";
-        else
-        cout<<ans<<endl;
-    }
-    
-    return 0;
-}
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
+	        if(ans==-1)
+	        cout<<"not present\n";
+	        else
+	        cout<<ans<<endl;
+	    }
+	    
+	    return 0;
+	}
